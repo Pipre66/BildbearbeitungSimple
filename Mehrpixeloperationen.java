0a3a4e2ad5ca8924d1e35a23ffe784cf26936b8a
pirmin <bothurpirmin@gmail.com>
2021-07-20 09:14:21 +0200

    letzte stunde

--- Mehrpixeloperationen.java
+++ Mehrpixeloperationen.java
@@ -19,13 +19,45 @@ public class Mehrpixeloperationen  implements Bildoperation
     @Override
     public Picture apply(Picture originalBild)
     {
-         return originalBild.copy();   
+        return originalBild.copy();   
     }
 
- 
 
-    public Picture beispieloperation(Picture originalBild )
+    public Picture faltung(Picture originalBild, double[] [] filter)
     {
-        return originalBild.copy(); 
+        int breite = originalBild.getWidth();
+        int hoehe = originalBild.getHeight();
+        Color[] [] pixel = originalBild.getPixelsColorTable();
+        Color[] [] pixelNeu = pixel.clone();
+        int laenge = filter.length;
+        int halb = laenge/2;
+        for( int x=halb; x<originalBild.getWidth() -halb; x++ ) {
+            for (int y=halb; y<originalBild.getHeight() - halb; y++) {
+                int xx = x-halb;
+                int yy = y-halb;
+                double rot = 0.0;
+                double gruen = 0.0;
+                double blau = 0.0;
+                for (int i=0; i<laenge; i++) {
+                    for (int j=0; j<laenge; j++) {
+                        rot += filter[i][j] * pixel[xx+i][yy+j].getRed();
+                        gruen += filter[i][j] * pixel[xx+i][yy+j].getGreen();
+                        blau += filter[i][j] * pixel[xx+i][yy+j].getBlue();
+                    }
+                }
+                if (rot < 0.0) rot = 0.0;
+                if (rot > 1.0) rot = 1.0;
+                if (gruen < 0.0) gruen = 0.0;
+                if (gruen > 1.0) gruen = 1.0;
+                if (blau < 0.0) blau = 0.0;
+                if (blau > 1.0) blau = 1.0;
+                pixelNeu[x][y] = new Color( rot, gruen, blau, 1.0);
+            }
+        }
+        Picture neuesBild = originalBild.copy();
+        neuesBild.setPixelsArray(pixelNeu);
+        return neuesBild;
     }
+
 }
+
